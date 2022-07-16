a649a308a37ea391d609d9008a29074d0124bd1b
Roberto Alsina <roberto.alsina@gmail.com>
2022-07-16 18:06:48 -0300

    Trtatando que ande

--- nombres/handler.py
+++ nombres/handler.py
@@ -1,8 +1,15 @@
+from collections import namedtuple as nt
 import pygal
 from json import loads
+from pony import orm
+import unicodedata
+import urllib
+import requests
+
+from data.data import comunes
 
 db = orm.Database()
-db.bind(provider="sqlite", filename="nombres.db", create_db=True)
+db.bind(provider="sqlite", filename="data/nombres.db", create_db=True)
 
 
 def remove_accents(input_str):
@@ -32,6 +39,83 @@ class TotalPorNombre(db.Entity):
         return f"{self.nombre}:{self.contador}"
 
 
+@orm.db_session
+def split_por_genero(nombres):
+    no_clasificados = set()
+    # Veamos cuales de estos nombres ya están clasificados
+    for nombre in nombres:
+        clasificador = remove_accents(
+            nombre.nombre.split()[0]
+        )  # genderize no aprecia acentos para AR
+        genero = Género.get(nombre=clasificador)
+        if not genero:
+            # No está clasificado
+            no_clasificados.add(urllib.parse.quote(clasificador))
+
+    if no_clasificados:
+        print(f"Tengo {len(no_clasificados)} sin clasificar")
+        no_clasificados = list(no_clasificados)
+        # Averiguar los no clasificados
+        # Partimos en bloques de a 10 (API de genderize)
+        for i in range(len(no_clasificados) // 10 + 1):
+            chunk = no_clasificados[i * 10 : (i + 1) * 10]
+            url = f'https://api.genderize.io/?name[]={"&name[]=".join(chunk)}&country_id=AR'
+            clasificados = requests.get(url)
+            for resultado in clasificados.json():
+                if not resultado["name"]:
+                    continue  # No me importa
+                if resultado["gender"] == "male":
+                    masc = resultado["probability"]
+                elif resultado["gender"] == "female":
+                    masc = 1 - resultado["probability"]
+                else:
+                    # Probablemente un acento o algo así
+                    print(f"Raro:{resultado}")
+                    masc = None
+                # Metemos en la base
+                print(f"Clasificando {resultado}: {masc}")
+                Género(nombre=resultado["name"], masculinidad=masc)
+
+    nombres_f = []
+    nombres_m = []
+    for nombre in nombres:
+        clasificador = remove_accents(nombre.nombre.split()[0])
+        genero = Género.get(nombre=clasificador)
+        if not genero or genero.masculinidad is None:  # No clasificado, en ambos
+            nombres_f.append(nombre)
+            nombres_m.append(nombre)
+        elif 0.4 < genero.masculinidad:
+            nombres_m.append(nombre)
+        elif 0.6 > genero.masculinidad:
+            nombres_f.append(nombre)
+    return {"f": nombres_f, "m": nombres_m}
+
+
+def datos_globales():
+    # Datos falsos
+    Dato = nt("Dato", ["contador", "nombre"])
+    return [Dato(contador=x[0], nombre=x[1]) for x in comunes][:50]
+
+
+@orm.db_session
+def datos_del_año(año):
+    # Nombres más populares de ese año
+    # Filtramos por prefijo
+    filtrado = orm.select(n for n in Dato if n.año == año).order_by(
+        orm.desc(Dato.contador)
+    )[:50]
+    return filtrado
+
+
+@orm.db_session
+def datos_por_prefijo(prefijo):
+    # Nombres por prefijo, sin importar el año
+    filtrado = orm.select(
+        n for n in TotalPorNombre if n.nombre.startswith(prefijo)
+    ).order_by(orm.desc(TotalPorNombre.contador))[:50]
+    return filtrado
+
+
 def handle(req):
     """handle a request to the function
     Args:
@@ -42,7 +126,7 @@ def handle(req):
     prefijo = data.get("p") or None
     genero = data.get("g") or None
     try:
-        año = int(request.args.get("a"))
+        año = int(data.get("a"))
     except Exception:
         año = None
 
@@ -75,8 +159,8 @@ def handle(req):
     elif len(datos) == 1:
         chart.title = f"¡Hola {datos[0].nombre.title()}!"
     elif len(datos) < 1:
-        chart.title = f"¡No esssistís!"
+        chart.title = "¡No esssistís!"
     chart.add("", [dato.contador for dato in datos[::-1]])
 
-    return Response(chart.render(is_unicode=True), mimetype="image/svg+xml")
-
+    # return Response(chart.render(is_unicode=True), mimetype="image/svg+xml")
+    return chart.render(is_unicode=True)
